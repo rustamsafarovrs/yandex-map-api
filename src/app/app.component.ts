271a45a830af3710dd9f35ed0d3583efda4c3986
rustamsafarovrs <rs.programmer@hotmail.com>
2021-05-31 15:09:30 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import ymaps from 'ymaps';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'yandex-map-api';
+export class AppComponent implements OnInit {
+  myMap;
+  yMaps;
+
+  ngOnInit(): void {
+    this.init();
+  }
+
+  private init(): void {
+    ymaps
+      .load('https://api-maps.yandex.ru/2.1/?apikey=1fcd605d-9dde-4bf2-92be-540cc8637956&lang=ru_RU&load=Map')
+      .then(maps => {
+        this.myMap = new maps.Map('my-map', {
+          center: [55.751574, 37.573856],
+          zoom: 9
+        });
+        this.myMap.events.add('click', (e) => {
+          this.myMap.geoObjects.removeAll();
+          const coords = e.get('coords');
+          const clickedPlace = new maps.Placemark(coords);
+          maps.geocode(coords).then((res) => {
+            const firstGeoObject = res.geoObjects.get(0);
+            console.log(firstGeoObject.getAddressLine());
+          });
+          this.myMap.geoObjects.add(clickedPlace);
+        });
+      })
+      .catch(error => console.log('Failed to load Yandex Maps', error));
+  }
+
 }
