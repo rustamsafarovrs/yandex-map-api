d4cb5951449096c35d56af69c2d80bb3d2383c31
rustamsafarovrs <rs.programmer@hotmail.com>
2021-06-01 10:59:22 +0500

    add detect location feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {YaEvent} from 'angular8-yandex-maps';
+import {YaEvent, YaReadyEvent} from 'angular8-yandex-maps';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +7,7 @@ import {YaEvent} from 'angular8-yandex-maps';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  map: ymaps.Map;
   point: number[];
   center: number[] = [55.76, 37.64];
   addressLine;
@@ -19,16 +20,25 @@ export class AppComponent implements OnInit {
     this.point = [55.847, 37.6];
   }
 
-  onYaClick($event: YaEvent<ymaps.Map>): void {
-    const coords = $event.event.get('coords');
-    this.point = [coords[0], coords[1]];
-  }
-
   onYaActionEnd($event: YaEvent<ymaps.Map>): void {
     this.point = $event.event.originalEvent.target.getCenter();
     $event.ymaps.geocode(this.point).then((res: any) => {
       const firstGeoObject = res.geoObjects.get(0);
       this.addressLine = firstGeoObject.getAddressLine();
     });
+  }
+
+  onMapReady($event: YaReadyEvent<ymaps.Map>): void {
+    this.map = $event.target;
+  }
+
+  onDetectLocationClick(): void {
+    ymaps.geolocation.get({
+      provider: 'browser',
+      mapStateAutoApply: true,
+    }).then((res) => {
+      console.log(res.geoObjects.get(0).properties.get('metaDataProperty'));
+    })
+      .catch(error => console.log('Yandex Map Error: ', error));
   }
 }
